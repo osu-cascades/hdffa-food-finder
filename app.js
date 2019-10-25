dca13bbb86d14cbac959bf13b72b9f1f022cd2d7
Andrew Bell <belland@oregonstate.edu>
2019-10-25 09:59:17 -0700

    Basic express server and HTML template, ref #1

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/src/index.html');
+})
+
+app.listen(port, () => {
+    console.log(`app running on port ${port}`)
+})
