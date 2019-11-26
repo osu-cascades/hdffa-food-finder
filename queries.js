61d0e48ea80e52ede9bca9ae92802b33e102a6df
Florentina Ilie <ilief@oregonstate.edu>
2019-11-26 12:17:50 -0800

    Added query functions

--- queries.js
+++ queries.js
@@ -5,3 +5,35 @@ const pool = new Pool({
 	password:'password',
 	port: 5432,
 })
+
+const db = require('./queries')
+
+const getPartners = (request, response)=> {
+	pool.query('SELECT * FROM places ORDER BY id ASC', (error, results)=> {
+		if(error) {
+			throw error
+		}
+		response.status(200).json(results.rows)
+	})
+}
+
+const getPartnersById = (request, response)=> {
+	pool.query('SELECT * FROM places WHERE id = $1', [id], (error, results)=> {
+		if(error) {
+			throw error
+		}
+		response.status(200).json(results.rows)
+	})
+}
+
+const createPartners = (request, response)=> {
+	const { name, email }
+} 
+
+module.exports = {
+	getUsers,
+	getUserById,
+	createUser,
+	updateUser,
+	deleteUser,
+}
