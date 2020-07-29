4a982ad8b0770de9b787e60bdf483f7c11a23794
Yong Bakos <ybakos@humanoriented.com>
2020-07-29 11:49:55 -0700

    Provider: Test entering lat and lon in feature

--- test/system/providers_test.rb
+++ test/system/providers_test.rb
@@ -1,19 +1,16 @@
 require "application_system_test_case"
 
 class ProvidersTest < ApplicationSystemTestCase
-  setup do
-    @provider = providers(:one)
-  end
-
   test "visiting the index" do
     visit providers_url
     assert_selector "h1", text: "Providers"
   end
 
   test "creating a Provider" do
-    visit providers_url
-    click_on "New Provider"
-    fill_in "Name", with: @provider.name
+    visit new_provider_url
+    fill_in "Name", with: 'FAKE'
+    fill_in "Latitude", with: 42
+    fill_in "Longitude", with: 42
     click_on "Create Provider"
     assert_text "Provider was successfully created"
   end
@@ -21,7 +18,9 @@ class ProvidersTest < ApplicationSystemTestCase
   test "updating a Provider" do
     visit providers_url
     click_on "Edit", match: :first
-    fill_in "Name", with: @provider.name
+    fill_in "Name", with: 'FAKE'
+    fill_in "Latitude", with: 42
+    fill_in "Longitude", with: 42
     click_on "Update Provider"
     assert_text "Provider was successfully updated"
   end
