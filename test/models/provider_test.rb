b9c8f4f8519c28dd5421b7fbdaeed246a96a9075
Yong Bakos <ybakos@humanoriented.com>
2020-07-29 11:34:36 -0700

    Provider: Test attributes and validator.

--- test/models/provider_test.rb
+++ test/models/provider_test.rb
@@ -6,4 +6,23 @@ class ProviderTest < ActiveSupport::TestCase
     Provider.new
   end
 
+  test 'Provider has a name' do
+    assert_respond_to(Provider.new, :name)
+  end
+
+  test 'Provider has a latitude' do
+    assert_respond_to(Provider.new, :latitude)
+  end
+
+  test 'Provider has a longitude' do
+    assert_respond_to(Provider.new, :longitude)
+  end
+
+  test 'Provider without a name is invalid' do
+    provider = Provider.new(name: 'Fake', latitude: 42, longitude: 42)
+    assert(provider.valid?)
+    provider.name = nil
+    refute(provider.valid?)
+  end
+
 end
