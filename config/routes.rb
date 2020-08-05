dbd73cee0539db0e554c90da7760aa79da50a7a3
Yong Joseph Bakos <ybakos@humanoriented.com>
2020-08-05 00:40:14 -0700

    Providers: Reroute home screen to public providers list.
    Add map from StaticPages#home to Providers#index view.
    Delete StaticPages#home.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root 'static_pages#home'
+  root 'providers#index'
 
   # Users
   # Using Devise RegistrationsController for public user creation/registration.
