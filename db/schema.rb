c0cce5e430a19865fdf51cad662369fc958bd2ac
Brayden Brown <braydenbrown@gmail.com>
2020-08-06 14:45:13 -0700

    Provider: Add index for making names unique and no duplication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_28_174236) do
+ActiveRecord::Schema.define(version: 2020_08_06_213950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_07_28_174236) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.index ["name"], name: "index_providers_on_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
