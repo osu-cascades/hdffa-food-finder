f788f7b0f1b9ace1f4b770512f3a7b450a0b2b3c
Yong Bakos <ybakos@humanoriented.com>
2020-08-11 15:04:07 -0700

    Provider: Remove generated fax_number

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,6 +1,5 @@
 class Provider < ApplicationRecord
   phony_normalize :phone, default_country_code: 'US'
-  phony_normalized_method :fax_number
   validates :name, presence: true, uniqueness: true
   validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :phone, phony_plausible: true
@@ -8,4 +7,5 @@ class Provider < ApplicationRecord
     format: { with: /\Ahttps?:\/\/([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?.*/,
     message: "must be an http or https URL"},
     allow_blank: true
+
 end
