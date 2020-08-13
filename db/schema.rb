40c6ac7c04e1ff5c02886c20ffa2c57c3aab7fbe
Brayden Brown <braydenbrown@gmail.com>
2020-08-12 21:20:40 -0700

    Categories: Add name to categories model and make it not null and unique with an index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_13_032959) do
+ActiveRecord::Schema.define(version: 2020_08_13_041236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_08_13_032959) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_categories_on_name", unique: true
   end
 
   create_table "providers", force: :cascade do |t|
