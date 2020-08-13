efad5f3ca0ef672b48a2e4d2b0725a413a0524c1
Brayden Brown <braydenbrown@gmail.com>
2020-08-13 14:28:24 -0700

    Categories: Add model validations and testing for name attribute

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def new_category
+    Category.new(name:'Fake')
+  end
+
+  test 'Cateegory has a name' do
+    assert_respond_to(Category.new, :name)
+  end
+
+  test 'Category without a name is invalid' do
+    category = new_category
+    assert category.valid?
+    category.name = nil
+    refute category.valid?
+  end
+
+  test 'Category with a non-unique name is invalid' do
+    existing_category_name = categories(:one).name
+    category = categories(:two)
+    assert category.valid?
+    category.name = existing_category_name
+    refute category.valid?
+  end
 end
