c519c283b7e42a460d7151950ceb00f4c8af31fb
Brayden Brown <braydenbrown@gmail.com>
2020-09-01 16:21:09 -0700

    Product, Partner: Create  many to many association

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,5 +1,6 @@
 class Partner < ApplicationRecord
   has_and_belongs_to_many :categories
+  has_and_belongs_to_many :products
   phony_normalize :phone, default_country_code: 'US'
   has_one_attached :logo
   validates :name, presence: true, uniqueness: true
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+  has_and_belongs_to_many :partners
   validates :name, presence: true, uniqueness: true
 
   def to_s
