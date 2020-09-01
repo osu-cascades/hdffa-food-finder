0041ad5861615ba7d3f8e50bed773e04606361b7
Brayden Brown <braydenbrown@gmail.com>
2020-09-01 15:34:14 -0700

    Product:Change admin and regular controller to match categories and partners

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,12 @@
 class ProductsController < ApplicationController
-end
+    skip_before_action :authenticate_user!
+
+    def index
+      @products = Product.all
+    end
+  
+    def show
+      @product = Product.find(params[:id])
+    end
+  
+  end
