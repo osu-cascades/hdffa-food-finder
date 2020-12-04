fb5fca597b9e68dd3fa4f7a5c2146ea8dafa2157
Yong Bakos <ybakos@humanoriented.com>
2020-12-03 22:18:10 -0800

    Admin::PartnersController: Permit keywords param.

--- app/controllers/admin/partners_controller.rb
+++ app/controllers/admin/partners_controller.rb
@@ -58,7 +58,7 @@ class Admin::PartnersController < ApplicationController
     def partner_params
       params.require(:partner).permit(:name, :longitude, :latitude, :description,
         :street_address, :city, :state, :zip, :email, :phone, :url, :logo, :product, :email, :description,
-        :hours_of_operation)
+        :hours_of_operation, :keywords)
     end
 
 end
