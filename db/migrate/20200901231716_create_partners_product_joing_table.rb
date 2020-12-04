4d44460fe2c865691c161c1e6a69bb10347d4b37
Yong Bakos <ybakos@humanoriented.com>
2020-12-03 22:36:27 -0800

    Migraitons: Fix spelling.

--- db/migrate/20200901231716_create_partners_product_joing_table.rb
+++ db/migrate/20200901231716_create_partners_product_joing_table.rb
@@ -1,4 +1,4 @@
-class CreatePartnersProductJoingTable < ActiveRecord::Migration[5.2]
+class CreatePartnersProductJoinTable < ActiveRecord::Migration[5.2]
   def change
     create_join_table :products, :partners do |t|
       t.index :product_id
