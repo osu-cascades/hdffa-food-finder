6eed531444d5f7ce67cfa920cb7b4e3241538624
KristenOrue <oruek@oregonstate.edu>
2020-12-05 13:49:12 -0800

    Delete old note.rb model and recreated it correctly from command line->migration has been automatically created

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,11 +1,2 @@
 class Note < ApplicationRecord
-    has_and_belongs_to_many :partners
-    has_and_belongs_to_many :users
-    validates :name, presence: true, uniqueness: true
-    attr_accessor :content 
-  
-    def to_s
-      name
-    end
-    
-  end
+end
