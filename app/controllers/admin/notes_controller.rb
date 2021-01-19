a998dcf8c346ff409e12f0db6a45a261d301f8f0
KristenOrue <oruek@oregonstate.edu>
2021-01-18 20:14:43 -0800

    Create a form for users to add a new note in the show.html.haml file

--- app/controllers/admin/notes_controller.rb
+++ app/controllers/admin/notes_controller.rb
@@ -1,65 +1,76 @@
 class Admin::NotesController < ApplicationController
+    before_action :restrict_unless_admin
 
-  def index
-    @notes = Note.all
-  end
-  
-  def show
-    @note = Note.find(params[:id])
-    respond_to do |format|
-      format.html
-      format.json {render json: @note }
+
+    def index
+      @notes = Note.all
     end
-  end
-
-  def new
-    @note = Note.new
-    @partner = Partner.find(params[:partner_id])
-    respond_to do |format|
-      format.html
-      format.json { render json: @note }
-    end
-  end
-
-  def edit
-    @note = Note.find(params[:id])
-    @partner = Partner.find(@note.partner_id)
-  end
-
-  def create
-    @partner = Partner.find(params[:partner_id])
-    @note = @partner.notes.build(params[:note])
-    respond_to do |format|
-      if @note.save
-        format.html { redirect_to @partner, notice: 'Note was successfully created.' }
-        format.json { render json: @note, status: :created, location: @note }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @note.errors, status: :unprocessable_entity }
+    
+    def show
+      @note = Note.find(params[:id])
+      respond_to do |format|
+        format.html
+        format.json {render json: @note }
       end
     end
-  end
 
-  def update
-    @note = Note.find(params[:id])
-    @partner = Partner.find(@note.partner_id)
-    respond_to do |format|
-      if @note.update_attributes(params[:note])
-        format.html { redirect_to @partner, notice: 'Note was successfully updated.' }
+    def new
+      @note = Note.new
+      @partner = Partner.find(params[:partner_id])
+      respond_to do |format|
+        format.html
+        format.json { render json: @note }
+      end
+    end
+
+    def edit
+      @note = Note.find(params[:id])
+      @partner = Partner.find(@note.partner_id)
+    end
+
+    def create
+      @partner = Partner.find(params[:partner_id])
+      @note = Note.new(note_params)
+      # @note = @partner.notes.build(note_params)
+      respond_to do |format|
+        if @note.save
+          format.html { redirect_to [:admin, @partner], notice: 'Note was successfully created.' }
+          format.json { render json: @note, status: :created, location: @note }
+        else
+          format.html { render :new }
+          # format.html { render action: "new" }
+          format.json { render json: @note.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+
+    def update
+      @note = Note.find(params[:id])
+      @partner = Partner.find(@note.partner_id)
+      respond_to do |format|
+        if @note.update_attributes(params[:note])
+          format.html { redirect_to @partner, notice: 'Note was successfully updated.' }
+          format.json { head :no_content }
+        else
+          format.html { render action: "edit" }
+          format.json { render json: @note.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+
+    def destroy
+      @note = Note.find(params[:id])
+      @note.destroy
+      respond_to do |format|
+        format.html { redirect_to :back }
         format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @note.errors, status: :unprocessable_entity }
       end
     end
-  end
 
-  def destroy
-    @note = Note.find(params[:id])
-    @note.destroy
-    respond_to do |format|
-      format.html { redirect_to :back }
-      format.json { head :no_content }
-    end
+    private
+
+      def note_params
+        params.require(:note).permit(:title, :body)
+      end
+
   end
-end
