f2bf3d85c0f5d7781c41a5f4abcb95498e10f09c
KristenOrue <oruek@oregonstate.edu>
2021-03-02 15:53:40 -0800

    Format the JSON to include a data: section matching the requested json format from chillbrodev

--- app/controllers/api/v1/partners_controller.rb
+++ app/controllers/api/v1/partners_controller.rb
@@ -8,6 +8,8 @@ class Api::V1::PartnersController < ApplicationController
   
     def show
       @partner = Partner.find(params[:id])
-      render json: @partner
+      # render json: { data: pretty_print(partner)}
+      # render json: { data: render_to_string('show.html.haml') }
+      render json: {data: @partner}
     end
 end
