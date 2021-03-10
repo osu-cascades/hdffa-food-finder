21475723c941886b9cde3ca9edd7c8e44db62e26
Marji Symonds <marji712@gmail.com>
2021-03-10 13:45:30 -0800

    Try staging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "hdffa_directory_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'hdffa-directory.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'hdffa-directory-staging.herokuapp.com' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
