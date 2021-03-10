e1a36ba286b438b2bdf608ebf5a37dae98f564c2
Marji Symonds <marji712@gmail.com>
2021-03-10 13:42:53 -0800

    Add smtp delivery method

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,11 +66,22 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "hdffa_directory_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'hdffa-directory-staging.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'hdffa-directory.herokuapp.com' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'hdffa-directory.herokuapp.com',
+    user_name:            'help@hdffa-directory.herokuapp.com',
+    password:             'password',
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
