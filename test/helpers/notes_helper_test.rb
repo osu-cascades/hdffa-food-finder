e2db3253d6e51ee254716e8792c984e067d9771b
Marji Symonds <marji712@gmail.com>
2021-04-08 13:53:44 -0700

    Stub and skip remaining helper tests needed

---
+++ test/helpers/notes_helper_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+
+    #make a dummy note with a created_at
+
+    test 'formats date and time for notes' do
+        skip  
+    end
+end
---
+++ test/helpers/users_helper_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+
+    test 'cannot edit if not editable_by_user' do 
+        skip
+    end
+
+    test 'can edit if editable_by_user' do
+        skip
+    end
+end
