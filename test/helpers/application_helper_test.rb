d8a63ae989d7fe6c5a7b935c5b405a4b751cedd9
Marji Symonds <marji712@gmail.com>
2021-04-08 13:30:42 -0700

    Application_Helper: update link definition to pass tests correctly

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -20,7 +20,7 @@ class ApplicationHelperTest < ActionView::TestCase
   end
 
   test 'displays sign out link when user is logged in' do
-    # skip("See https://stackoverflow.com/questions/53514537/how-to-test-a-rails-5-helper-that-relies-on-devise-signed-in-helper-with-minit")
+    # ("See https://stackoverflow.com/questions/53514537/how-to-test-a-rails-5-helper-that-relies-on-devise-signed-in-helper-with-minit")
     @signed_in = true
     assert_match 'Sign Out', sign_in_or_out_link
   end
