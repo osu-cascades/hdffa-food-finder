bdd504040edf1fb010907d5dc306782e87f9fd20
Marji Symonds <marji712@gmail.com>
2021-04-08 13:18:09 -0700

    Application Helper: update for use with devise

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -14,10 +14,10 @@ class ApplicationHelperTest < ActionView::TestCase
     '/users/new'
   end
 
-  # test 'displays sign in link when user is not logged in' do
-  #   @signed_in = false
-  #   assert_match 'Sign In', sign_in_or_out_link
-  # end
+  test 'displays sign in link when user is not logged in' do
+    @signed_in = false
+    assert_match 'Sign In', sign_in_or_out_link
+  end
 
   test 'displays sign out link when user is logged in' do
     # skip("See https://stackoverflow.com/questions/53514537/how-to-test-a-rails-5-helper-that-relies-on-devise-signed-in-helper-with-minit")
