eea0f39de73f1747679ea116b187bc6f568ad6c0
Marji Symonds <marji712@gmail.com>
2021-04-22 16:01:48 -0700

    Add case for no featured listing References #126

--- app/helpers/admin/featured_listings_helper.rb
+++ app/helpers/admin/featured_listings_helper.rb
@@ -1,6 +1,10 @@
 module Admin::FeaturedListingsHelper
     def featured_listing_name(featured_listing_id)
-        listing = FeaturedListing.find_by(id: featured_listing_id)
-        listing.name
+        if featured_listing_id
+            listing = FeaturedListing.find_by(id: featured_listing_id)
+            listing.name
+        else
+            "none"
+        end
     end
 end
