c1e4af569782d722451ad1702979d96bec75e7ca
Marji Symonds <marji712@gmail.com>
2021-04-22 15:59:06 -0700

    Add helper to find featured listing name by id References #126

--- app/helpers/admin/featured_listings_helper.rb
+++ app/helpers/admin/featured_listings_helper.rb
@@ -1,2 +1,6 @@
 module Admin::FeaturedListingsHelper
+    def featured_listing_name(featured_listing_id)
+        listing = FeaturedListing.find_by(id: featured_listing_id)
+        listing.name
+    end
 end
