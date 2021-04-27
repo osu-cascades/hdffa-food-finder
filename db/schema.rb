960a353359aad6e1cc60e6ef25b368ef6a9ddfbf
Marji Symonds <marji712@gmail.com>
2021-04-27 14:54:00 -0700

    Add dietary model with relationships References #130

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_22_223420) do
+ActiveRecord::Schema.define(version: 2021_04_27_215231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,12 @@ ActiveRecord::Schema.define(version: 2021_04_22_223420) do
     t.bigint "partner_id", null: false
     t.index ["category_id"], name: "index_categories_partners_on_category_id"
     t.index ["partner_id"], name: "index_categories_partners_on_partner_id"
+  end
+
+  create_table "dietaries", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "featured_listings", force: :cascade do |t|
