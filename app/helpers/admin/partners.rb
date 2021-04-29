7eeed44bb10ed3327521ee06163173454baaaf6c
Marji Symonds <marji712@gmail.com>
2021-04-29 14:36:11 -0700

    Remove address partner helper

--- app/helpers/admin/partners.rb
+++
@@ -1,9 +0,0 @@
-module Admin::PartnersHelper
-    def partner_address_provided(partner)?
-        if partner.street_address || partner.city
-            true
-        else
-            false
-        end
-    end
-end
