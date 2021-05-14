f5aea7d2ce9e8c6c7c21b65fa2b38422bf4266a5
Michael Tornatta <michaeltornatta@gmail.com>
2021-05-14 02:54:37 -0700

    New admin feature test, searching. Perform a blank search

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,14 +2,6 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
 
-  # def signed_in?
-  #   false
-  # end
-
-  # test 'displays sign in link when user is not logged in' do
-  #   assert_match "Sign In", sign_in_or_out_link
-  # end
-
   def signed_in?
     @signed_in
   end
---
+++ test/system/admin/search_test.rb
@@ -0,0 +1,48 @@
+require 'application_system_test_case'
+
+class AdminSearchTest < ApplicationSystemTestCase
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    sign_in users(:admin)
+  end
+
+  test 'performing a blank search' do
+    visit search_path
+    assert_text 'Search Results'
+  end
+
+  # test 'viewing a list of Categories' do
+  #   visit admin_categories_url
+  #   assert_text 'Categories'
+  #   assert_text categories(:one).name
+  # end
+
+  # test 'viewing a Category' do
+  #   category = categories(:one)
+  #   visit admin_category_url(category)
+  #   assert_text category.name
+  # end
+
+  # test 'creating a Category' do
+  #   visit new_admin_category_url
+  #   fill_in 'Name', with: 'FAKE'
+  #   click_on 'Create Category'
+  #   assert_text 'Category was successfully created'
+  # end
+
+  # test 'updating a Category' do
+  #   visit edit_admin_category_url(categories(:one))
+  #   fill_in 'Name', with: 'NEW FAKE NAME'
+  #   click_on 'Update Category'
+  #   assert_text 'Category was successfully updated'
+  #   assert_text 'NEW FAKE NAME'
+  # end
+
+  # test 'destroying a Category' do
+  #   visit admin_categories_url
+  #   click_on 'Delete', match: :first
+  #   assert_text 'Category was successfully destroyed'
+  # end
+
+end
