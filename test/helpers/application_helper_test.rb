16ef4da916c8d2ccb62eb4513291b86f97b4ebb1
Michael Tornatta <michaeltornatta@gmail.com>
2021-05-14 02:23:52 -0700

    Added Sign Out test to application helper test. Sign In test still not working

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,12 +2,34 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
 
+  # def signed_in?
+  #   false
+  # end
+
+  # test 'displays sign in link when user is not logged in' do
+  #   assert_match "Sign In", sign_in_or_out_link
+  # end
+
   def signed_in?
-    false
+    @signed_in
   end
 
-  test 'displays sign in link when user is not logged in' do
-    assert_match 'Sign In', sign_in_or_out_link
+  def new_user_session_path
+    '/users/new'
+  end
+
+  def destroy_user_session_path
+    '/users/new'
+  end
+
+  test 'returns Sign Out when signed in' do
+    @signed_in = true
+    assert_match 'Sign Out', sign_in_or_out_link
+  end
+
+  test 'returns Sign In when not signed in' do
+    @signed_in = false
+    assert_match /Sign In/, sign_in_or_out_link
   end
 
   test 'displays sign out link when user is logged in' do
