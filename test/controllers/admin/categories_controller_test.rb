207a88c6ee29f0a0db9646466273b887b5fb76f2
Marji Symonds <marji712@gmail.com>
2021-05-20 15:17:01 -0700

    Fix notes and categories controller test misname

--- test/controllers/admin/categories_controller_test.rb
+++ test/controllers/admin/categories_controller_test.rb
@@ -1,66 +1,66 @@
 require 'test_helper'
 
-class AdminNotesControllerTest < ActionDispatch::IntegrationTest
+class AdminCategoriesControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
   test 'requires user authentication' do
-    assert(defines_before_filter?(Admin::NotesController, :authenticate_user!))
+    assert(defines_before_filter?(Admin::CategoriesController, :authenticate_user!))
   end
 
   test 'redirects requests from unauthenticated sessions' do
     # index
-    get admin_notes_path
+    get admin_categories_path
     assert_redirected_to new_user_session_path
     # show
-    get admin_note_path(id: 'fake')
+    get admin_category_path(id: 'fake')
     assert_redirected_to new_user_session_path
     # new
-    get new_admin_note_path
+    get new_admin_category_path
     assert_redirected_to new_user_session_path
     # edit
-    get edit_admin_note_path(id: 'fake')
+    get edit_admin_category_path(id: 'fake')
     assert_redirected_to new_user_session_path
     # create
-    post admin_notes_path
+    post admin_categories_path
     assert_redirected_to new_user_session_path
     # update
-    patch admin_note_path(id: 'fake')
+    patch admin_category_path(id: 'fake')
     assert_redirected_to new_user_session_path
-    put admin_note_path(id: 'fake')
+    put admin_category_path(id: 'fake')
     assert_redirected_to new_user_session_path
     # destroy
-    delete admin_note_path(id: 'fake')
+    delete admin_category_path(id: 'fake')
     assert_redirected_to new_user_session_path
   end
 
   test 'restricts guest user access' do
-    assert defines_before_filter?(Admin::NotesController, :restrict_unless_admin)
+    assert defines_before_filter?(Admin::CategoriesController, :restrict_unless_admin)
   end
 
   test 'redirects requests from guest users to root url' do
     sign_in users(:guest)
     # index
-    get admin_notes_path
+    get admin_categories_path
     assert_redirected_to root_url
     # show
-    get admin_note_path(id: 'fake')
+    get admin_category_path(id: 'fake')
     assert_redirected_to root_url
     # new
-    get new_admin_note_path
+    get new_admin_category_path
     assert_redirected_to root_url
     # edit
-    get edit_admin_note_path(id: 'fake')
+    get edit_admin_category_path(id: 'fake')
     assert_redirected_to root_url
     # create
-    post admin_notes_path
+    post admin_categories_path
     assert_redirected_to root_url
     # update
-    patch admin_note_path(id: 'fake')
+    patch admin_category_path(id: 'fake')
     assert_redirected_to root_url
-    put admin_note_path(id: 'fake')
+    put admin_category_path(id: 'fake')
     assert_redirected_to root_url
     # destroy
-    delete admin_note_path(id: 'fake')
+    delete admin_category_path(id: 'fake')
     assert_redirected_to root_url
   end
 
